9ef2d2520c4743b987903562bc7122431eecb1fa
Rakshitih <rakshithkumab01718@gmail.com>
2025-11-19 21:22:56 +0530

    QuizContext for playing Quiz with Start, Submitanswer, reset the game

--- frontend/src/context/QuizContext.jsx
+++ frontend/src/context/QuizContext.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { createContext, useState } from "react";
 
-const QuizContext = () => {
-  return (
-    <div>QuizContext</div>
-  )
-}
 
-export default QuizContext
+export const QuizContext = createContext();
+
+export const QuizProvider = ({ children}) => {
+
+  const [currentQuiz, setCurrentQuiz] = useState(null);
+  const [answer, setAnswer] = useState([]);
+  const [score, setScore] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+// start the Quiz
+  const startQuiz = (quiz) => {
+    setCurrentQuiz(quiz);
+    setAnswer([]);
+    setScore(null);
+  };
+
+  //Submitting the answer
+  const submitAnswer = (questionId, answer ) => {
+    setAnswer([...answer, { questionId, answer}]);
+  };
+
+  // reset to play again
+  const resetQuiz = () => {
+    setCurrentQuiz(null);
+    setAnswer([]);
+    setScore(null);
+  };
+
+
+  return(
+    <QuizContext.Provider 
+    value={{
+      currentQuiz,
+      answer,
+      score,
+      loading,
+      setLoading,
+      startQuiz,
+      submitAnswer,
+      setScore,
+      resetQuiz,
+    }}
+    >{children}
+    </QuizContext.Provider>
+  );
+
+
+};
