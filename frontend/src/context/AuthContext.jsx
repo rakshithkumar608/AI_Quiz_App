7f17350ef6398a26782a227d841e0f5b9ac2f717
Rakshitih <rakshithkumab01718@gmail.com>
2025-11-19 20:58:07 +0530

    The AuthContex added to maintain login, logout for users and token

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
 
-const AuthContext = () => {
+import {  createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children}) => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
+
+  const login = (userData, token) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+    localStorage.setItem("token", token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+
+  };
+
   return (
-    <div>AuthContext</div>
+    <AuthContext.Provider value={{ user, login, logout}}>
+      {children}
+    </AuthContext.Provider>
   )
 }
-
-export default AuthContext
