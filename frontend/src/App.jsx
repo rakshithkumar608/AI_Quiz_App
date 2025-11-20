cacfb8592d64cce8c88d876d8ba101726abb48af
Rakshitih <rakshithkumab01718@gmail.com>
2025-11-20 21:57:44 +0530

    Added the paths Routes , Route, Routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { QuizProvider } from "./context/QuizContext";
+import Home from "./pages/Home";
+import LoginForm from "./components/Auth/LoginForm";
+import RegisterForm from "./components/Auth/RegisterForm";
+import Dashboard from "./pages/Dashboard";
+import GamePage from "./pages/GamePage";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <AuthProvider>
+      <QuizProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/quiz/:id" element={<GamePage />} />
+          </Routes>
+        </Router>
+      </QuizProvider>
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
