8b1b6a27a394c30f3d1a93a6ce0673203bf3f539
Rakshitih <rakshithkumab01718@gmail.com>
2025-11-20 21:27:11 +0530

    Toast UI added for notifications toast

---
+++ frontend/src/components/UI/Toast.jsx
@@ -0,0 +1,31 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import React, { useEffect } from 'react';
+
+const Toast = ({ message, type = "info", show, setShow }) => {
+  useEffect(() => {
+    if (show) {
+      const timer = setTimeout(() => setShow(false), 3000);
+      return() => clearTimeout(timer);
+    }
+  }, [show, setShow]);
+
+  const bgColor = type === "success" ?
+  "bg-green-500" : type === "error" ?
+  "bg-red-500" : "bg-blue-500";
+
+  return (
+   <AnimatePresence>
+    {show && (
+      <motion.div initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0}} 
+      exit={{ opacity: 0, y: 50 }}
+      className={`fixed bottom-5 right-5 text-white px-6 py-3 rounded shadow-lg
+       ${bgColor}`}>
+      {message}
+      </motion.div>
+    )}
+   </AnimatePresence>
+  )
+}
+
+export default Toast
